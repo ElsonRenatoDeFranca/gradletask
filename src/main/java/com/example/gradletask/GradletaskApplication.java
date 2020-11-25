9a24e33bad3682120bfd4df53343301aaf6252af
ElsonRenatoDeFranca <elson.renato.de.franca@gmail.com>
2020-11-24 23:43:00 -0300

    first commit

---
+++ src/main/java/com/example/gradletask/GradletaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradletask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradletaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradletaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gradletask/GradletaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradletask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradletaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
